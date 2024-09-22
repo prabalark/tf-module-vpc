87d37ae285a018cd359c0aaa60d8bf20680cb96d
prabala <radhakrishnaprabala8@gmail.com>
2024-09-22 22:51:05 +0530

    vars.tf vr

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr_block  # var1 keep in envdev-main.tfvars
   enable_dns_support = true
   enable_dns_hostnames = true
-  tags = merge(var.tags, {name="${var.env}"})
+  tags = merge(var.tags, {Name="${var.env}-vpc"})
 }
