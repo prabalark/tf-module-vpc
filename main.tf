584543bb71bb5416f03508574615f1ffb6e91244
prabala <radhakrishnaprabala8@gmail.com>
2024-09-25 22:36:30 +0530

    main.tf vr2

--- main.tf
+++ main.tf
@@ -36,8 +36,8 @@ resource "aws_nat_gateway" "ngw" {
   count = length(var.subnets["public"]"cidr_block") #multiple ngw are taking
   allocation_id = aws_eip.ngw[count.index].id   #elip linking
   subnet_id     = module.subnets["public"].subnet_ids[count.index] #need to get list of subnets
-  tags          = merge(var.tags, {Name="${var.env}-ngw"}) #to get name
-  }
+  tags  = merge(var.tags, {Name="${var.env}-ngw"}) #to get name
+}
 
 
 #output "vpc" {
