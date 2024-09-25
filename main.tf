c0ca70d8334d8bb6abbbbabb482ebc415678206d
prabala <radhakrishnaprabala8@gmail.com>
2024-09-25 23:03:50 +0530

    main.tf vr2

--- main.tf
+++ main.tf
@@ -45,9 +45,3 @@ resource "aws_nat_gateway" "ngw" {
 #  value = "module.vpc"
 #}
 
-resource "aws_route_table" "main" {
-  count = length(var.cidr_block)
-  vpc_id = aws_vpc.main.id
-
-  tags  = merge(var.tags, {Name="${var.env}-${var.name}-rt-${count.index+1}"}) #to get name
-}
--- vars.tf
+++ vars.tf
@@ -2,4 +2,3 @@ variable "cidr_block" {}
 variable "tags" {}
 variable "env" {}
 variable "subnets" {}
-variable "name" {}
