5313095bb6836e415fca4293dac9b87f1d5067d1
prabala <radhakrishnaprabala8@gmail.com>
2024-09-25 22:02:47 +0530

    main.tf vr2

--- main.tf
+++ main.tf
@@ -31,10 +31,10 @@ resource "aws_eip" "ngw" {
   tags  = merge(var.tags, {Name="${var.env}-ngw"}) #to get name
 }
 
-need to get list of subnets ,so 1st do outputs.tf
-resource "aws_nat_gateway" "ngw" {
-  count = length(var.subnets["public"]"cidr_block") #multiple ngw are taking
-  allocation_id = aws_eip.ngw[count.index].id   #elip linking
-  subnet_id     = module.subnets["public"].subnet_ids[count.index] #need to get list of subnets
-  tags          = merge(var.tags, {Name="${var.env}-ngw"}) #to get name
-  }
+#need to get list of subnets ,so 1st do outputs.tf
+#resource "aws_nat_gateway" "ngw" {
+#  count = length(var.subnets["public"]"cidr_block") #multiple ngw are taking
+#  allocation_id = aws_eip.ngw[count.index].id   #elip linking
+#  subnet_id     = module.subnets["public"].subnet_ids[count.index] #need to get list of subnets
+#  tags          = merge(var.tags, {Name="${var.env}-ngw"}) #to get name
+#  }
