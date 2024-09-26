8b84a3c4d7e6cfecadba1fe42945454ef0710c0f
prabala <radhakrishnaprabala8@gmail.com>
2024-09-26 23:28:02 +0530

    main.tf vr2

--- main.tf
+++ main.tf
@@ -53,9 +53,9 @@ resource "aws_route" "rtprigw" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route" "rtprigw" {
-  count                  = length(module.subnets["public"].route_table_ids)
-  route_table_id         = module.subnets["public"].route_table_ids[count.index]
-  nat_gateway_id         = aws_internet_gateway.igw.id
+resource "aws_route" "rtprngw" {
+  count                  = length(local.all_private_subnet_ids)
+  route_table_id         = local.all_private_subnet_ids[count.index]
+  nat_gateway_id         = element(aws_nat_gateway.ngw.*.id,count.index)
   destination_cidr_block = "0.0.0.0/0"
 }
