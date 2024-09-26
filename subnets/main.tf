dcc9527c1fff4f421cbc74cfb1567d6ecb123bbb
prabala <radhakrishnaprabala8@gmail.com>
2024-09-26 21:37:10 +0530

    outputs.tf vr2

--- subnets/main.tf
+++ subnets/main.tf
@@ -23,8 +23,8 @@ resource "aws_route_table_association" "associate" {
 
 # This step we will do in route table creation in manual step
 resource "aws_route" "rtprigw" {
-  count =  length(module.subnets["public"].route_table_ids)
-  route_table_id = module.subnets["public"].route_table_ids[count.index]
+  count                  = length(module.subnets["public"].route_table_ids)
+  route_table_id         = module.subnets["public"].route_table_ids[count.index]
   gateway_id             = aws_internet_gateway.igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
