fb336807d08bb76796b781c902a121b0e40e0788
prabala <radhakrishnaprabala8@gmail.com>
2024-10-06 18:42:22 +0530

    main.tf vr2

--- main.tf
+++ main.tf
@@ -65,14 +65,14 @@ resource "aws_vpc_peering_connection" "peer" {
   auto_accept = true
 }
 
-resource "aws_route" "peering_connection-route" {
+resource "aws_route" "peering-connection-route" {
   count = length(local.all_private_subnet_ids)
   route_table_id = element(local.all_private_subnet_ids, count.index)
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   destination_cidr_block    = var.default_vpc_cidr
 }
 
-resource "aws_route" "peering_connection-route-in-default-vpc" {
+resource "aws_route" "peering-connection-route-in-default-vpc" {
   route_table_id            = var.default_vpc_rtid
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   destination_cidr_block    = var.cidr_block
